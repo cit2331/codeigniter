89a8f947dbabbbde80046419225ddc67a6f5e703
cit2331 <tranvanhieubmt001@gmail.com>
2025-04-16 08:37:45 +0700

    16042025

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+//   /restaurants => tên đường dẫn tự đặt
+/// Home => Controller
+/// ::restaurants => hàm trong controller
+$routes->get('/res', 'Home::restaurants');
